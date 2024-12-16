b6f674d7451d2c1de278dcda3f19b7a670409658
dmsft <dmsft@outlook.dk>
2024-12-16 11:31:01 +0100

    Update data.py

--- paragraphvec/data.py
+++ paragraphvec/data.py
@@ -6,7 +6,6 @@ from math import ceil
 
 import numpy as np
 import torch
-from numpy.random import choice
 
 import numpy as np
 from collections import Counter
@@ -17,50 +16,53 @@ from paragraphvec.utils import DATA_DIR
 
 class CustomDataset(Dataset):
 
-    def __init__(self, file_path:str):
+    def __init__(self, file_path=""):
     
-        self.counter = Counter()
-        self.examples = []
-        self.vocab = dict()
-        self.vocab_size = 0
-    
-        with open(file_path, 'r', encoding='utf-8') as f:
-            next(f)  # Skip header
-    
-            for line in f:
-                text = line.strip()
-                tokens = _tokenize_str(text)
-                
-                self.examples.append(tokens)
-    
-        self.build_vocab()
+        (self.TotalWords, self.LinesTokens) = self.load_file(file_path)
+        self.WordFreq = self.count_words()
+        (self.VocabSize, self.Vocabulary) = self.build_vocab()
 
     
+    def load_file(self, file_path:str):
+        """Parse lines from a text file into a tokens list."""
+
+        with open(file_path, 'r', encoding='utf-8') as fd:
+            next(fd)  # Skip header
+            toks = [Tokenize(line) for line in fd]
+        
+        return (sum(len(tk) for tk in toks), toks)
+    
+
+    def count_words(self):
+        """Count word frequencies in the dataset."""
+
+        wf = Counter()
+        for sent_toks in self.LinesTokens:
+            wf.update(sent_toks)
+        
+        return wf
+    
+
     def build_vocab(self):
+        """Build a vocabulary from the word frequencies."""        
 
-        for tokens in self.examples:
-            self.counter.update(tokens)
+        vocab = {word: idx for (idx, word) in enumerate(self.WordFreq.keys())}
+        return (len(vocab), vocab)
         
-        self.vocab = {word: idx for idx, word in enumerate(self.counter.keys())}
-        self.vocab_size = len(self.vocab)
-        
-        #self.stoi = self.vocab  # string to index mapping
-        #self.itos = {idx: word for word, idx in self.vocab.items()}  # index to string mapping
-
     
     def __len__(self):
-        return len(self.examples)
+        return len(self.LinesTokens)
 
     
     def __getitem__(self, idx):
     
-        tokens = self.examples[idx]
-        token_ids = [self.vocab[token] for token in tokens if token in self.vocab]
+        tokens = self.LinesTokens[idx]
+        token_ids = [self.Vocabulary[token] for token in tokens if token in self.Vocabulary]
     
         return token_ids
 
 
-def load_dataset(file_name:str):
+def LoadDataset(file_name:str):
     """Loads contents from a file in the *data* directory into a
     torchtext.data.TabularDataset instance.
     """
@@ -70,32 +72,34 @@ def load_dataset(file_name:str):
     return dataset
 
 
-def _tokenize_str(str_):
-    
-    # keep only alphanumeric and punctations
-    str_ = re.sub(r'[^A-Za-z0-9(),.!?\'`]', ' ', str_)
-    
-    # remove multiple whitespace characters
-    str_ = re.sub(r'\s{2,}', ' ', str_)
-    
-    # punctations to tokens
-    str_ = re.sub(r'\(', ' ( ', str_)
-    str_ = re.sub(r'\)', ' ) ', str_)
-    str_ = re.sub(r',', ' , ', str_)
-    str_ = re.sub(r'\.', ' . ', str_)
-    str_ = re.sub(r'!', ' ! ', str_)
-    str_ = re.sub(r'\?', ' ? ', str_)
-    
-    # split contractions into multiple tokens
-    str_ = re.sub(r'\'s', ' \'s', str_)
-    str_ = re.sub(r'\'ve', ' \'ve', str_)
-    str_ = re.sub(r'n\'t', ' n\'t', str_)
-    str_ = re.sub(r'\'re', ' \'re', str_)
-    str_ = re.sub(r'\'d', ' \'d', str_)
-    str_ = re.sub(r'\'ll', ' \'ll', str_)
+def Tokenize(line="") -> list[str]:
     
     # lower case
-    return str_.strip().lower().split()
+    txt = line.strip().lower()
+
+    # keep only alphanumeric and punctations
+    txt = re.sub(r'[^A-Za-z0-9(),.!?\'`]', ' ', line)
+    
+    # remove multiple whitespace characters
+    txt = re.sub(r'\s{2,}', ' ', txt)
+    
+    # punctations to tokens
+    txt = re.sub(r'\(', ' ( ', txt)
+    txt = re.sub(r'\)', ' ) ', txt)
+    txt = re.sub(r',', ' , ', txt)
+    txt = re.sub(r'\.', ' . ', txt)
+    txt = re.sub(r'!', ' ! ', txt)
+    txt = re.sub(r'\?', ' ? ', txt)
+    
+    # split contractions into multiple tokens
+    txt = re.sub(r'\'s', ' \'s', txt)
+    txt = re.sub(r'\'ve', ' \'ve', txt)
+    txt = re.sub(r'n\'t', ' n\'t', txt)
+    txt = re.sub(r'\'re', ' \'re', txt)
+    txt = re.sub(r'\'d', ' \'d', txt)
+    txt = re.sub(r'\'ll', ' \'ll', txt)
+    
+    return txt.split()
 
 
 class NCEData(object):
@@ -127,33 +131,37 @@ class NCEData(object):
         of machine CPUs is used.
     """
     # code inspired by parallel generators in https://github.com/fchollet/keras
-    def __init__(self, dataset, batch_size, context_size,
-                 num_noise_words, max_size, num_workers):
-        self.max_size = max_size
+    def __init__(self, dataset:CustomDataset, batch_size, context_size,
+                 num_noise_words, max_size, num_workers=0):
 
-        self.num_workers = num_workers if num_workers != -1 else os.cpu_count()
-        if self.num_workers is None:
+        if not num_workers:
             self.num_workers = 1
+        elif num_workers == -1:
+            self.num_workers = os.cpu_count()
+        else:
+            self.num_workers = num_workers
 
-        self._generator = _NCEGenerator(
-            dataset,
-            batch_size,
-            context_size,
-            num_noise_words,
-            _NCEGeneratorState(context_size))
+        self._generator = _NCEGenerator(dataset, batch_size,
+            context_size, num_noise_words, _NCEGeneratorState(context_size)
+        )
 
+        self.max_size = max_size
         self._queue = None
         self._stop_event = None
         self._processes = []
 
+
     def __len__(self):
         return len(self._generator)
+    
 
     def vocabulary_size(self):
         return self._generator.vocabulary_size()
 
+
     def start(self):
         """Starts num_worker processes that generate batches of data."""
+
         self._queue = multiprocessing.Queue(maxsize=self.max_size)
         self._stop_event = multiprocessing.Event()
 
@@ -163,7 +171,9 @@ class NCEData(object):
             self._processes.append(process)
             process.start()
 
+
     def _parallel_task(self):
+
         while not self._stop_event.is_set():
             try:
                 batch = self._generator.next()
@@ -172,11 +182,14 @@ class NCEData(object):
             except KeyboardInterrupt:
                 self._stop_event.set()
 
+    
     def get_generator(self):
-        """Returns a generator that yields batches of data."""
+        """A generator that yields batches of data."""
+        
         while self._is_running():
             yield self._queue.get()
 
+    
     def stop(self):
         """Terminates all processes that were created with start()."""
         if self._is_running():
@@ -204,44 +217,45 @@ class _NCEGenerator(object):
 
     Parameters
     ----------
-    state: paragraphvec.data._NCEGeneratorState
+    `state`: paragraphvec.data._NCEGeneratorState
         Initial (indexing) state of the generator.
 
     For other parameters see the NCEData class.
     """
-    def __init__(self, dataset, batch_size, context_size, num_noise_words, state):
+    def __init__(self, dataset:CustomDataset, batch_size, context_size, num_noise_words:int, state):
 
         self.dataset = dataset
         self.batch_size = batch_size
         self.context_size = context_size
-        self.num_noise_words = num_noise_words
+        self._num_noise_words = num_noise_words
 
-        self._vocabulary = self.dataset.vocab
-        self._vocab_size = self.dataset.vocab_size
-        self._init_noise_distribution()
         self._state = state
+        self._vocabulary = self.dataset.Vocabulary
+        self._vocab_size = self.dataset.VocabSize
+        self._noise_distribution = self._init_noise_distribution()
 
     
     def _init_noise_distribution(self):
+
         # we use a unigram distribution raised to the 3/4rd power,
         # as proposed by T. Mikolov et al. in Distributed Representations
         # of Words and Phrases and their Compositionality
         freqs = np.zeros(self._vocab_size)
 
-        for word, freq in self.dataset.counter.items():
+        for (word, freq) in self.dataset.WordFreq.items():
             idx = self._word_to_index(word)
             freqs[idx] = freq
 
         probs = np.power(freqs, 0.75)
         probs /= np.sum(probs)
-        self._noise_distribution = probs
-
-
-    def _sample_noise(self):
         
-        return choice(
-            self._vocab_size, self.num_noise_words, p=self._noise_distribution
-            ).tolist()
+        return probs
+
+
+    def _sample_noise(self) -> list[int]:
+        
+        x = np.random.choice(self._vocab_size, self._num_noise_words, p=self._noise_distribution)
+        return x.tolist()
         
 
     def __len__(self):
@@ -257,7 +271,7 @@ class _NCEGenerator(object):
         """Updates state for the next process in a process-safe manner
         and generates the current batch."""
 
-        prev_doc_id, prev_in_doc_pos = self._state.update_state(
+        (prev_doc_id, prev_in_doc_pos) = self._state.update_state(
             self.dataset,
             self.batch_size,
             self.context_size,
@@ -268,10 +282,9 @@ class _NCEGenerator(object):
 
         while len(batch) < self.batch_size:
             if prev_doc_id == len(self.dataset):
-                batch.torch_()
-                return batch
+                break
         
-            tokens = self.dataset.examples[prev_doc_id]
+            tokens = self.dataset.LinesTokens[prev_doc_id]
         
             if prev_in_doc_pos <= (len(tokens) - 1 - self.context_size):
                 self._add_example_to_batch(prev_doc_id, prev_in_doc_pos, batch)
@@ -286,7 +299,7 @@ class _NCEGenerator(object):
 
     def _num_examples_in_doc(self, doc_id=0, in_doc_pos=None):
     
-        tokens = self.dataset.examples[doc_id]
+        tokens = self.dataset.LinesTokens[doc_id]
     
         if in_doc_pos is not None:
             if len(tokens) - in_doc_pos >= self.context_size + 1:
@@ -302,7 +315,7 @@ class _NCEGenerator(object):
 
     def _add_example_to_batch(self, doc_id, in_doc_pos, batch):
         
-        tokens = self.dataset.examples[doc_id]
+        tokens = self.dataset.LinesTokens[doc_id]
         batch.doc_ids.append(doc_id)
 
         # Sample from the noise distribution
